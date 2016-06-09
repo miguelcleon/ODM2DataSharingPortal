abd302fd6a4bb1f1f9719612d765b5d5f0aaf420
Juan <juan.caraballo17@gmail.com>
2016-06-09 13:25:22 -0600

    Added device registration model to the data loader web interface app.

--- src/WebSDL/dataloaderinterface/models.py
+++ src/WebSDL/dataloaderinterface/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class DeviceRegistration(models.Model):
+    registration_id = models.AutoField(primary_key=True, db_column='RegistrationID')
+    deployment_sampling_feature_uuid = models.UUIDField(db_column='SamplingFeatureUUID')
+    authentication_token = models.CharField(max_length=64, editable=False, db_column='AuthToken')
+    user = models.OneToOneField(User, db_column='User')
