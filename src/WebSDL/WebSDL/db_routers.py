627b25446dbcfb9e8eeb1049dadf9356f0a23a02
Juan <juan.caraballo17@gmail.com>
2016-06-14 15:03:28 -0600

    Added tests for the authentication views (registration, logout, etc.), updated older tests.

--- src/WebSDL/WebSDL/db_routers.py
+++ src/WebSDL/WebSDL/db_routers.py
@@ -3,15 +3,19 @@ import sys
 
 class WebSDLRouter(object):
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'dataloader':
+        is_testing = 'test' in sys.argv
+        if model._meta.app_label == 'dataloader' or is_testing:
             return 'odm2'
         return 'default'
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label in ['dataloader', 'dataloaderservices']:
+        is_testing = 'test' in sys.argv
+        if model._meta.app_label in ['dataloader', 'dataloaderservices'] or is_testing:
             return 'odm2'
         return 'default'
 
     def allow_migrate(self, db, app_label, **hints):
-        allow = app_label not in ['dataloader', 'dataloaderservices'] or 'test' in sys.argv
+        is_testing = 'test' in sys.argv
+        unmanaged_apps = ['dataloader']
+        allow = app_label not in unmanaged_apps or is_testing
         return allow
--- src/WebSDL/dataloaderinterface/tests/test_views.py
+++ src/WebSDL/dataloaderinterface/tests/test_views.py
@@ -1,37 +1,129 @@
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from dataloaderinterface.forms import UserRegistrationForm
 from dataloaderinterface.views import *
 
 # Create your tests here.
 
 
+class TestAuthentication(TestCase):
+    @staticmethod
+    def get_test_user_data():
+        return {
+            'username': 'preparetodie', 'password1': 'test_password', 'password2': 'test_password',
+            'first_name': 'Inigo', 'last_name': 'Montoya'
+        }
+
+    @staticmethod
+    def create_user():
+        user_data = TestAuthentication.get_test_user_data()
+        del user_data['password1']
+        del user_data['password2']
+        return User.objects.get_or_create(**user_data)[0]
+
+    def setUp(self):
+        pass
+
+    def test_valid_registration_form(self):
+        form = UserRegistrationForm(data=self.get_test_user_data())
+        self.assertTrue(form.is_valid())
+
+    def test_empty_registration_form(self):
+        form = UserRegistrationForm(data={})
+        self.assertFalse(form.is_valid())
+
+    def test_nonmatching_passwords_registration_form(self):
+        data = self.get_test_user_data()
+        data['password1'] = '.'
+        data['password2'] = '-'
+        form = UserRegistrationForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_registration_view(self):
+        redirect_location = reverse('home')
+        url = reverse('user_registration')
+        response = self.client.post(url, self.get_test_user_data(), follow=True)
+        self.assertRedirects(response, expected_url=redirect_location)
+        self.assertTrue(User.objects.filter(username=self.get_test_user_data()['username']).exists())
+
+    def test_empty_registration_view(self):
+        redirect_location = reverse('home')
+        url = reverse('user_registration')
+        response = self.client.post(url, {}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/register.html')
+
+    def test_logout(self):
+        url = reverse('logout')
+        user = self.create_user()
+        self.client.force_login(user)
+        response = self.client.get(url)
+        redirect_location = reverse('home')
+        self.assertNotIn('_auth_user_id', self.client.session)
+        self.assertRedirects(response, expected_url=redirect_location)
+
+    def tearDown(self):
+        user = User.objects.filter(username='preparetodie')
+        if user.exists():
+            user.delete()
+
+
 class TestDevicesListView(TestCase):
+    def setUp(self):
+        self.user = TestAuthentication.create_user()
+        self.client.force_login(self.user)
+
     def test_status(self):
         url = reverse('devices_list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_list_rendering(self):
-        return True
+        pass
+
+    def tearDown(self):
+        self.user.delete()
 
 
 class TestDeviceDetailView(TestCase):
+    def setUp(self):
+        self.user = TestAuthentication.create_user()
+        self.client.force_login(self.user)
+
     def test_status(self):
         url = reverse('device_detail', args=[1])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
+    def tearDown(self):
+        self.user.delete()
+
 
 class TestDeviceRegistrationView(TestCase):
+    def setUp(self):
+        self.user = TestAuthentication.create_user()
+        self.client.force_login(self.user)
+
     def test_status(self):
         url = reverse('device_registration')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
+    def tearDown(self):
+        self.user.delete()
+
 
 class TestDeviceUpdateView(TestCase):
+    def setUp(self):
+        self.user = TestAuthentication.create_user()
+        self.client.force_login(self.user)
+
     def test_status(self):
         url = reverse('device_update', args=[1])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+    def tearDown(self):
+        self.user.delete()
