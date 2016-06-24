fb88049883e9cdeda11582eae8232900084c0d08
Juan <juan.caraballo17@gmail.com>
2016-06-24 11:42:54 -0600

    Fixed formset initialization and made everything more preeteeee

--- src/WebSDL/WebSDL/settings/base.py
+++ src/WebSDL/WebSDL/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'djangoformsetjs',
     'crispy_forms',
+    'bootstrapform',
     'dataloader',
     'dataloaderservices',
     'dataloaderinterface'
