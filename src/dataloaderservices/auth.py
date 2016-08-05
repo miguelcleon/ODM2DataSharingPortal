28224343cbd097900940e37e6f8a425dbc57aeeb
unknown <stephanie.reeder@usu.edu>
2016-08-05 10:46:43 -0600

    update error messages

--- src/dataloaderservices/auth.py
+++ src/dataloaderservices/auth.py
@@ -6,17 +6,17 @@ from dataloaderinterface.models import DeviceRegistration
 
 
 class UUIDAuthentication(authentication.BaseAuthentication):
-    def authenticate(self, request):
-        # TODO: Get auth_token(uuid) from header, get registration object with auth_token, get the user from that registration, verify sampling_feature uuid is registered by this user, be happy.
-        token = request.META['HTTP_TOKEN']
-        registration_queryset = DeviceRegistration.objects.using('default').filter(authentication_token=token)
-        if not registration_queryset.exists():
-            raise exceptions.AuthenticationFailed('teh')
-        registration = registration_queryset.get()
-
-        # request needs to have the sampling feature uuid of the registration - TODO: there's probably a better way of comparing this
-        if str(registration.deployment_sampling_feature_uuid) != request.data[u'sampling_feature']:
-            raise exceptions.AuthenticationFailed('blah')  # or other related exception
-
-        # TODO: probably log user in? (registration.user)
-        return None
+	def authenticate(self, request):
+		# TODO: Get auth_token(uuid) from header, get registration object with auth_token, get the user from that registration, verify sampling_feature uuid is registered by this user, be happy.
+		token = request.META['HTTP_TOKEN']
+		registration_queryset = DeviceRegistration.objects.using('default').filter(authentication_token=token)
+		if not registration_queryset.exists():
+			raise exceptions.AuthenticationFailed('teh')
+		registration = registration_queryset.get()
+		
+		# request needs to have the sampling feature uuid of the registration - TODO: there's probably a better way of comparing this
+		if str(registration.deployment_sampling_feature_uuid) != request.data['sampling_feature']:
+			raise exceptions.AuthenticationFailed('blah')  # or other related exception
+			
+		# TODO: probably log user in? (registration.user)
+		return None
