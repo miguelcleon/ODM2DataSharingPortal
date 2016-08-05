56abf1d4bcf332b8340f3549128e40c8a1a37119
sreeder <stephanie.reeder@usu.edu>
2016-08-05 15:21:44 -0600

    update error messages and datetime conversion

--- src/dataloaderservices/auth.py
+++ src/dataloaderservices/auth.py
@@ -7,16 +7,16 @@ from dataloaderinterface.models import DeviceRegistration
 
 class UUIDAuthentication(authentication.BaseAuthentication):
 	def authenticate(self, request):
-		# TODO: Get auth_token(uuid) from header, get registration object with auth_token, get the user from that registration, verify sampling_feature uuid is registered by this user, be happy.
+		#  Get auth_token(uuid) from header, get registration object with auth_token, get the user from that registration, verify sampling_feature uuid is registered by this user, be happy.
 		token = request.META['HTTP_TOKEN']
 		registration_queryset = DeviceRegistration.objects.using('default').filter(authentication_token=token)
 		if not registration_queryset.exists():
 			raise exceptions.PermissionDenied('Invalid Security Token')
 		registration = registration_queryset.get()
 		
-		# request needs to have the sampling feature uuid of the registration - TODO: there's probably a better way of comparing this
+		# request needs to have the sampling feature uuid of the registration - 
 		if str(registration.deployment_sampling_feature_uuid) != request.data['sampling_feature']:
-			raise exceptions.AuthenticationFailed('Invalid Site Identifier')  # or other related exception
+			raise exceptions.AuthenticationFailed('Site Identifier is not associated with this Token')  # or other related exception
 			
-		# TODO: probably log user in? (registration.user)
+
 		return None
