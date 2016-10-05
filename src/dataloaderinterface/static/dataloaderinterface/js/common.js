0baf9c32b8bc1cd718b76d0f61131c636671b537
Juan <juan.caraballo17@gmail.com>
2016-10-04 19:52:47 -0600

    Fixed empty latitude/longitude requests error.

--- src/dataloaderinterface/static/dataloaderinterface/js/common.js
+++ src/dataloaderinterface/static/dataloaderinterface/js/common.js
@@ -3,21 +3,27 @@
 //
 // }());
 
-
-function updateSitePosition() {
-
+function updateSitePosition(map, position) {
+    map.panTo(position);
+    $('input[name="latitude"]').val(position.lat);
+    $('input[name="longitude"]').val(position.lng);
 }
 
-function updateSiteElevation() {
-
+function updateSiteElevation(position) {
+    var elevator = new google.maps.ElevationService();
+     elevator.getElevationForLocations({'locations':[position]}, function(results, status) {
+          if (status == google.maps.ElevationStatus.OK && results[0]) {
+              $('input[name="elevation_m"]').val(results[0].elevation);
+          }
+     });
 }
 
 function initMap() {
-    var defaultZoomLevel = 5;
-    var defaultPosition = { latitude: -37.0902, longitude: 95.7129 };
+    var defaultZoomLevel = 4;
+    var defaultPosition = { lat: 37.0902, lng: -95.7129 };
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: defaultPosition.latitude, lng: defaultPosition.longitude },
+        center: defaultPosition,
         zoom: defaultZoomLevel,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
@@ -30,23 +36,35 @@ function initMap() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function getLocation(position){
             map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
-            map.setZoom(10)
+            map.setZoom(10);
         }, undefined, { timeout: 5000 });
     }
 
     map.addListener('click', function(event) {
-        var elevationUrl = [
-            'https://maps.googleapis.com/maps/api/elevation/json?locations=',
-            marker.position, '&key=AIzaSyAiUxiuZaPEPtB4EhAquguz7kNzYU7bnnc'
-        ].join('');
-
         marker.setPosition(event.latLng);
-        map.panTo(marker.position);
-        $.ajax({
-            url: elevationUrl
-        }).done(function(data) {
-            updateSiteElevation()
-        })
+        updateSitePosition(map, event.latLng);
+        updateSiteElevation(event.latLng);
     });
 
+    $('input[name="latitude"], input[name="longitude"]').on('change', function() {
+        var input = $(this);
+        var value = parseFloat(input.val());
+        if (!value) {
+            return;
+        }
+
+        var markerPosition = {
+            lat: input.prop('name') === 'latitude'? parseFloat(value): marker.getPosition().lat(),
+            lng: input.prop('name') === 'longitude'? parseFloat(value): marker.getPosition().lng()
+        };
+
+        map.panTo(markerPosition);
+        marker.setPosition(markerPosition);
+    });
 }
+
+$(document).on("keypress", ":input:not(textarea):not([type=submit])", function(event) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+    }
+});
