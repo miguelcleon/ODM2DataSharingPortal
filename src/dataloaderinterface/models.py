dc4d33329a2da23a9eadfd2c01ab7d89c8d38b12
Kenny Fryar-Ludwig <fryarludwig@gmail.com>
2016-11-14 13:34:20 -0700

    Small cosmetic changes to /devices page

--- src/dataloaderinterface/models.py
+++ src/dataloaderinterface/models.py
@@ -14,6 +14,24 @@ class DeviceRegistration(models.Model):
     authentication_token = models.CharField(max_length=64, editable=False, db_column='AuthenticationToken')
     user = models.ForeignKey(User, db_column='User')
 
+    def registration_date(self):
+        sampling_feature = SamplingFeature.objects.using('odm2').get(
+            sampling_feature_uuid__exact=self.deployment_sampling_feature_uuid)
+        result = str(sampling_feature.feature_action.get().action).split(' ')
+        d = result[3].replace('-', '/')
+        t = result[4].split('+')[0]
+        return d + ' ' + t
+
+    def device_name(self):
+        sampling_feature = SamplingFeature.objects.using('odm2').get(
+                sampling_feature_uuid__exact=self.deployment_sampling_feature_uuid)
+        return sampling_feature.sampling_feature_code
+
     def __str__(self):
-        sampling_feature = SamplingFeature.objects.using('odm2').get(sampling_feature_uuid__exact=self.deployment_sampling_feature_uuid)
-        return sampling_feature.sampling_feature_code + ' ' + str(sampling_feature.feature_action.get().action)
+        sampling_feature = SamplingFeature.objects.using('odm2').get(
+                sampling_feature_uuid__exact=self.deployment_sampling_feature_uuid)
+        result = str(sampling_feature.feature_action.get().action).split(' ')
+        d = result[3].replace('-', '/')
+        instr = '{}\t{} {}: {}'.format(sampling_feature.sampling_feature_code, result[0], result[1], d)
+
+        return instr
