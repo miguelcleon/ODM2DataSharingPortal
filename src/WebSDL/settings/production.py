14a1281a15ee8b00d8c1f3066266de756e2f0299
Juan <juan.caraballo17@gmail.com>
2016-12-12 17:17:34 -0700

    Added production server settings file. Why wasn't it there before? ¯\_(ツ)_/¯

---
+++ src/WebSDL/settings/production.py
@@ -0,0 +1,14 @@
+import django
+from WebSDL.settings.base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', data['host']]
+
+SITE_ROOT = os.environ['APPL_PHYSICAL_PATH']
+SITE_URL = os.environ['APPL_VIRTUAL_PATH'] + '/'
+
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+STATIC_URL = SITE_URL + 'static/'
+
+django.setup()
