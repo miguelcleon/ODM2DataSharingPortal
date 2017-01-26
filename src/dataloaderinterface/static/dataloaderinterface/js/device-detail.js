a407efa12535e18f799f0bdd28445c8ed1d34d78
mauriel133 <mauriel.ramirez@gmail.com>
2017-01-25 19:26:50 -0700

    Styling

--- src/dataloaderinterface/static/dataloaderinterface/js/device-detail.js
+++ src/dataloaderinterface/static/dataloaderinterface/js/device-detail.js
@@ -8,6 +8,7 @@ function initMap() {
 
     var map = new google.maps.Map(document.getElementById('map'), {
         center: sitePosition,
+        scrollwheel: false,
         zoom: defaultZoomLevel,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
