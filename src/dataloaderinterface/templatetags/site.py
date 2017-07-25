ede3659316f0777497983aa9a38d352739ddcb69
Juan <juan.caraballo17@gmail.com>
2017-07-25 12:48:53 -0600

    #18 Wrote template filter to get csv file path from result.

--- src/dataloaderinterface/templatetags/site.py
+++ src/dataloaderinterface/templatetags/site.py
@@ -1,6 +1,7 @@
-from dataloader.models import SamplingFeature
+from dataloader.models import SamplingFeature, Result
 from django import template
 
+from dataloaderinterface.csv_serializer import SiteResultSerializer
 from dataloaderinterface.models import DeviceRegistration
 
 register = template.Library()
@@ -12,6 +13,14 @@ def get_registration(sampling_feature):
         return
 
     return DeviceRegistration.objects.filter(deployment_sampling_feature_uuid__exact=sampling_feature.sampling_feature_uuid).first()
+
+
+@register.filter(name='get_result_csv_path')
+def get_result_csv_path(result):
+    if not isinstance(result, Result):
+        return
+
+    return SiteResultSerializer(result).get_file_path()
 
 
 @register.filter(name='can_administer_site')
