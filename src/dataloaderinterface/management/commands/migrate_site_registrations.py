84c4c4a49319b49a7a1ba46e2abcacd55124b363
Juan <juan.caraballo17@gmail.com>
2017-09-05 10:23:44 -0600

    Created new Site Registration backend structure.

---
+++ src/dataloaderinterface/management/commands/migrate_site_registrations.py
@@ -0,0 +1,74 @@
+from django.core.management.base import BaseCommand
+
+from dataloader.models import SamplingFeature
+from dataloaderinterface.models import DeviceRegistration, SiteRegistration, SiteSensor
+
+
+class Command(BaseCommand):
+    help = 'Fix corrupted or incomplete odm2 data in the scope of this application.'
+
+    @staticmethod
+    def create_site_registration(device_registration):
+        existing_site_registration = SiteRegistration.objects.filter(registration_token=device_registration.authentication_token).first()
+        if existing_site_registration:
+            return existing_site_registration
+
+        affiliation = device_registration.user.affiliation
+        sampling_feature = device_registration.sampling_feature
+
+        registration_data = {
+            'registration_token': device_registration.authentication_token,
+            'registration_date': device_registration.registration_date(),
+            'deployment_date': device_registration.deployment_date,
+            'django_user': device_registration.user.user,
+            'affiliation_id': device_registration.user.affiliation_id,
+            'person': str(affiliation.person),
+            'organization': str(affiliation.organization),
+            'sampling_feature_id': sampling_feature.sampling_feature_id,
+            'sampling_feature_code': sampling_feature.sampling_feature_code,
+            'sampling_feature_name': sampling_feature.sampling_feature_name,
+            'elevation_m': sampling_feature.elevation_m,
+            'site_latitude': sampling_feature.site.latitude,
+            'site_longitude': sampling_feature.site.longitude,
+            'site_type': sampling_feature.site.site_type_id
+        }
+
+        site_registration = SiteRegistration(**registration_data)
+        site_registration.save()
+        return site_registration
+
+    @staticmethod
+    def create_site_sensor(result, site_registration):
+        existing_site_sensor = SiteSensor.objects.filter(result_id=result.result_id).first()
+        if existing_site_sensor:
+            return existing_site_sensor
+
+        sensor_data = {
+            'result_id': result.result_id,
+            'registration': site_registration,
+            'variable_name': result.variable.variable_name_id,
+            'variable_code': result.variable.variable_code,
+            'unit_name': result.unit.unit_name,
+            'unit_abbreviation': result.unit.unit_abbreviation
+        }
+
+        site_sensor = SiteSensor(**sensor_data)
+        site_sensor.save()
+        return site_sensor
+
+    def handle(self, *args, **options):
+        sampling_features = SamplingFeature.objects.all()
+        print("%s sites found." % sampling_features.count())
+        print("----------------------------------------------------")
+
+        for sampling_feature in sampling_features:
+            device_registration = DeviceRegistration.objects.filter(deployment_sampling_feature_uuid=sampling_feature.sampling_feature_uuid).first()
+            if not device_registration:
+                print('**** Sampling Feature %s (%s) exists without a site registration!!!' % (sampling_feature.sampling_feature_id, sampling_feature.sampling_feature_code))
+                continue
+
+            site_registration = self.create_site_registration(device_registration)
+            for feature_action in sampling_feature.feature_actions.all():
+                result = feature_action.results.first()
+                self.create_site_sensor(result, site_registration)
+            print('- site %s migrated!' % sampling_feature.sampling_feature_code)
--- src/dataloaderinterface/models.py
+++ src/dataloaderinterface/models.py
@@ -8,6 +8,53 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class SiteRegistration(models.Model):
+    registration_id = models.AutoField(primary_key=True, db_column='RegistrationID')
+    registration_token = models.CharField(max_length=64, editable=False, db_column='RegistrationToken')
+
+    registration_date = models.DateTimeField(db_column='RegistrationDate')
+    deployment_date = models.DateTimeField(db_column='DeploymentDate', blank=True, null=True)
+
+    django_user = models.ForeignKey(User, on_delete=models.CASCADE, db_column='User')
+    affiliation_id = models.IntegerField(db_column='AffiliationID')
+    person = models.CharField(max_length=765, db_column='Person')
+    organization = models.CharField(max_length=255, db_column='Organization', blank=True, null=True)
+
+    sampling_feature_id = models.IntegerField(db_column='SamplingFeatureID')
+    sampling_feature_code = models.CharField(max_length=50, unique=True, db_column='SamplingFeatureCode')
+    sampling_feature_name = models.CharField(max_length=255, blank=True, db_column='SamplingFeatureName')
+    elevation_m = models.FloatField(blank=True, null=True, db_column='Elevation')
+
+    site_latitude = models.FloatField(db_column='Latitude')
+    site_longitude = models.FloatField(db_column='Longitude')
+    site_type = models.CharField(max_length=765, db_column='SiteType')
+
+    def __str__(self):
+        return '%s by %s from %s on %s' % (self.sampling_feature_code, self.person, self.organization, self.registration_date)
+
+    def __repr__(self):
+        return "<SiteRegistration('%s', '%s', '%s', '%s')>" % (
+            self.registration_id, self.registration_date, self.sampling_feature_code, self.person
+        )
+
+
+class SiteSensor(models.Model):
+    registration = models.ForeignKey('SiteRegistration', db_column='RegistrationID')
+    result_id = models.IntegerField(db_column='ResultID')
+    variable_name = models.CharField(max_length=255, db_column='VariableName')
+    variable_code = models.CharField(max_length=50, db_column='VariableCode')
+    unit_name = models.CharField(max_length=255, db_column='UnitsName')
+    unit_abbreviation = models.CharField(max_length=255, db_column='UnitAbbreviation')
+
+    def __str__(self):
+        return '%s %s' % (self.variable_name, self.unit_abbreviation)
+
+    def __repr__(self):
+        return "<SiteSensor('%s', [%s], '%s', '%s')>" % (
+            self.id, self.registration, self.variable_code, self.unit_abbreviation,
+        )
+
+
 class DeviceRegistration(models.Model):
     registration_id = models.AutoField(primary_key=True, db_column='RegistrationID')
     deployment_sampling_feature_uuid = models.UUIDField(db_column='SamplingFeatureUUID')
