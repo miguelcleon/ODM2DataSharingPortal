e433887592c767f60123fb6ae14bd046f9dcb11c
Mauriel <mauriel.ramirez@gmail.com>
2017-09-12 11:02:01 -0600

    Disabled annoying Ctrl + zoom for Site Details map

--- src/dataloaderinterface/static/dataloaderinterface/js/device-detail.js
+++ src/dataloaderinterface/static/dataloaderinterface/js/device-detail.js
@@ -10,7 +10,7 @@ function initMap() {
 
     var map = new google.maps.Map(document.getElementById('map'), {
         center: sitePosition,
-        scrollwheel: false,
+        gestureHandling: 'greedy',
         zoom: defaultZoomLevel,
         mapTypeId: google.maps.MapTypeId.SATELLITE
     });
