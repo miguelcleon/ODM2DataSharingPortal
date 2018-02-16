1822b71b9ab3e40b8851a84124f1eefcc90ab42e
darksinge <cr.blackburn89@gmail.com>
2018-02-16 15:21:32 -0700

    added workaround for datetime issue with site_sensor.last_measurement_utc_datetime

--- src/dataloaderservices/views.py
+++ src/dataloaderservices/views.py
@@ -25,6 +25,8 @@ from dataloaderinterface.models import SiteSensor, SiteRegistration
 from dataloaderservices.auth import UUIDAuthentication
 from dataloaderservices.serializers import OrganizationSerializer
 
+from pytz import utc
+
 
 class ResultApi(APIView):
     authentication_classes = (SessionAuthentication,)
@@ -221,6 +223,9 @@ class TimeSeriesValuesApi(APIView):
             site_sensor.last_measurement_datetime = result_value.value_datetime
             site_sensor.last_measurement_utc_offset = result_value.value_datetime_utc_offset
             site_sensor.last_measurement_utc_datetime = result_value.value_datetime - timedelta(hours=result_value.value_datetime_utc_offset)
+
+            if site_sensor.last_measurement_utc_datetime.tzinfo is None:
+                site_sensor.last_measurement_utc_datetime = utc.localize(result_value.value_datetime - timedelta(hours=result_value.value_datetime_utc_offset))
 
             if is_first_value:
                 result.valid_datetime = measurement_datetime
