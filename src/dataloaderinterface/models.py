e9c7cdc75633be469155715527d6c7f0c3648060
darksinge <cr.blackburn89@gmail.com>
2018-04-18 16:26:31 -0600

    added leafpack models

--- src/dataloaderinterface/models.py
+++ src/dataloaderinterface/models.py
@@ -15,6 +15,7 @@ from dataloader.models import SamplingFeature, Affiliation, Result, TimeSeriesRe
 from django.contrib.auth.models import User
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
 
 HYDROSHARE_SYNC_TYPES = (('M', 'Manual'), ('S', 'Scheduled'))
 
@@ -50,7 +51,6 @@ class SiteRegistration(models.Model):
     @property
     def odm2_affiliation(self):
         return Affiliation.objects.get(pk=self.affiliation_id)
-
 
     def __str__(self):
         return '%s by %s from %s on %s' % (self.sampling_feature_code, self.person, self.organization, self.registration_date)
@@ -378,3 +378,61 @@ class SiteAlert(models.Model):
         return "<SiteAlert('%s', [%s], '%s', '%s')>" % (
             self.id, self.site_registration.sampling_feature_code, self.last_alerted, self.hours_threshold,
         )
+
+
+class Macroinvertebrate(models.Model):
+    """
+    :var: scientific_name: The scientific name for the macroinvertebrate (i.e. 'Ephemeroptera')
+    :var: common_name: The common for the macroinvertebrate (i.e. 'mayflies')
+    :var: family_of: Establishes an Order-Family one-to-many relationships. For example, in biological classification,
+     Diptera is the order of all true flies and includes the families of Anthericidae, Chironomidae, Simuliidae, etc.,
+     but all are macroinvertebrate.
+    """
+    uuid = models.UUIDField()
+    scientific_name = models.CharField(max_length=255)  # i.e.
+    common_name = models.CharField(max_length=255)  #
+    family_of = models.ForeignKey('Macroinvertebrate', on_delete=models.CASCADE, null=True, blank=True)
+
+
+class LeafPack(models.Model):
+    """
+    Leaf pack data
+    """
+    site_registration = models.ForeignKey(SiteRegistration, on_delete=models.CASCADE)
+    uuid = models.UUIDField()
+    placement_date = models.DateField()
+    retrieval_date = models.DateField()
+    leaf_pack_count = models.IntegerField()
+    placement_air_temp = models.FloatField()
+    placement_water_temp = models.FloatField()
+    retrieval_air_temp = models.FloatField()
+    retrieval_water_temp = models.FloatField()
+    had_storm = models.BooleanField(default=False)
+    had_flood = models.BooleanField(default=False)
+    had_drought = models.BooleanField(default=False)
+    storm_count = models.IntegerField(default=0)
+    storm_precipiation = models.FloatField(default=0)
+    types = models.ManyToManyField('LeafPackType')
+    bugs = models.ManyToManyField(Macroinvertebrate, through='LeafPackBug')
+
+
+class LeafPackBug(models.Model):
+    """
+    This model defines the many-to-many relationship between LeafPack and Macroinvertebrate models
+    :var: leaf_pack: A LeafPack
+    :var: bug: A Macroinvertebrate... 'bug' is easier to spell :)
+    :var: bug_count: The number of associated macroinvertebrate counted in the associated leaf pack
+    """
+    leaf_pack = models.ForeignKey(LeafPack, on_delete=models.CASCADE)
+    bug = models.ForeignKey(Macroinvertebrate, on_delete=models.CASCADE)
+    bug_count = models.IntegerField(default=0)
+
+
+class LeafPackType(models.Model):
+    """
+    A simple class to store a leaf pack type (i.e. beech, birch, elm, gum, maple, pine, etc.)
+
+    Will remove the need to hard code leaf pack types, make choice fields easier to populate choices for, and leave the
+    management of adding/removing/editing leaf pack types to administrators/users.
+    """
+    name = models.CharField(max_length=255)
