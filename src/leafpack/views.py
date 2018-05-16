18e369a0a60914feb08c42aed7bb5a6d76d52afc
darksinge <cr.blackburn89@gmail.com>
2018-05-16 12:53:22 -0600

    removed default values for populating leafpack form

--- src/leafpack/views.py
+++ src/leafpack/views.py
@@ -130,31 +130,11 @@ class LeafPackCreateView(LeafPackUpdateCreateMixin, CreateView):
 
         context['sampling_feature_code'] = self.kwargs[self.slug_field]
 
-        # TODO: Remove this random leaf pack type generator stuff - for testing only...
-        # lpt_ids = [lp.id for lp in LeafPackType.objects.all()]
-        # rand_len = random.randint(1, 5)
-        # rand_lpts = list()
-        # while len(rand_lpts) < rand_len:
-        #     lpt = random.randint(1, len(lpt_ids))
-        #     if lpt in lpt_ids and lpt not in rand_lpts:
-        #         rand_lpts.append(lpt)
-
         if 'leafpack_form' in context:
             context['leafpack_form'] = context.pop('leafpack_form')
         else:
-            # TODO: Remove mock values (placement date, retrieval date, etc.) - they are for testing
             context['leafpack_form'] = LeafPackForm(initial={
                 'site_registration': SiteRegistration.objects.get(sampling_feature_code=self.kwargs[self.slug_field]),
-                # 'placement_date': date.today() - timedelta(days=7),
-                # 'retrieval_date': date.today(),
-                # 'placement_air_temp': 75.0,
-                # 'placement_water_temp': 75.0,
-                # 'retrieval_air_temp': 75.0,
-                # 'retrieval_water_temp': 75.0,
-                # 'leafpack_retrieval_count': 1,
-                # 'leafpack_placement_count': 1,
-                # 'storm_precipitation': 0,
-                # 'types': LeafPackType.objects.filter(reduce(OR, [Q(id=i) for i in rand_lpts]))
             })
 
         context['bug_count_form_list'] = LeafPackBugFormFactory.formset_factory()
@@ -164,8 +144,6 @@ class LeafPackCreateView(LeafPackUpdateCreateMixin, CreateView):
     def post(self, request, *args, **kwargs):
         leafpack_form = self.get_form()
         bug_forms = self.get_bug_count_forms()
-
-        # leafpack_form.data['storm_precipitation'] = 0
 
         if self.forms_valid([leafpack_form] + bug_forms):
             leafpack_form.save()
