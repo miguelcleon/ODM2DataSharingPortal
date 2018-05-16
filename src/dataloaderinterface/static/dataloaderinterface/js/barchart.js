bc33f061fff0a0cffcc3d21f91e67bcfca5e0c49
Mauriel <mauriel.ramirez@gmail.com>
2018-05-16 10:13:27 -0600

    Plot responsive behavior

--- src/dataloaderinterface/static/dataloaderinterface/js/barchart.js
+++ src/dataloaderinterface/static/dataloaderinterface/js/barchart.js
@@ -68,53 +68,88 @@ for (var i = 0; i < data.length; i++) {
     );
 }
 
-plot(data);
+// Plot the data
+x.domain(data.map(function (d) {
+    return d.taxon;
+}));
 
-function plot(data) {
-    x.domain(data.map(function (d) {
-        return d.taxon;
-    }));
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+    .append("text")
+    .attr("class", "label")
+    .style("text-anchor", "middle")
+    .attr("x", width / 2)
+    .attr("y", margin.bottom / 1.5)
+    .text("Taxonomic Group");
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("% of Total Individuals");
+
+svg.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function (d) {
+        return x(d.taxon);
+    })
+    .attr("width", x.rangeBand())
+    .attr("y", function (d) {
+        return y(d.frequency);
+    })
+    .attr("fill", function (d, i) {
+        return c20(i);
+    })
+    .attr("height", function (d) {
+        return height - y(d.frequency);
+    })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide);
+
+// Define responsive behavior
+function resize() {
+    width = $(".svg-container").width() - margin.left - margin.right;
+
+    // Update the range of the scale with new width/height
+    x.rangeRoundBands([0, width], .1);
+
+    // Update the axis and text with the new scale
+    svg.select(".x.axis")
         .call(xAxis)
-        .append("text")
-        .style("text-anchor", "middle")
-        .attr("x", width / 2 )
-        .attr("y", 30 )
-        .text("Taxonomic Group");
+        .attr("transform", "translate(0," + height + ")")
+        .select(".label")
+        .attr("transform", "translate(" + width / 2 + "," + margin.bottom / 1.5 + ")");
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("% of Total Individuals");
+    svg.select(".y.axis")
+        .call(yAxis);
 
+    // Force D3 to recalculate and update the line
     svg.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function (d) {
-            return x(d.taxon);
-        })
         .attr("width", x.rangeBand())
         .attr("y", function (d) {
             return y(d.frequency);
         })
-        .attr("fill", function (d, i) {
-            return c20(i);
+        .attr("x", function (d) {
+            return x(d.taxon);
         })
         .attr("height", function (d) {
             return height - y(d.frequency);
         })
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide)
-}
+};
+
+// Call the resize function whenever a resize event occurs
+d3.select(window).on('resize', resize);
+
+// Call the resize function
+resize();
 
 function type(d) {
     d.frequency = +d.frequency;
