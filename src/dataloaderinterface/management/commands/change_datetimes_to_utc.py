6a0dafccdbc07669c0983912a6b756c3a554f57d
jcaraballo17 <juan.caraballo17@gmail.com>
2018-06-06 13:47:18 -0400

    #234 Added script to convert all data value datetimes to utc time.

---
+++ src/dataloaderinterface/management/commands/change_datetimes_to_utc.py
@@ -0,0 +1,17 @@
+from datetime import timedelta
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from dataloader.models import TimeSeriesResultValue
+
+
+class Command(BaseCommand):
+    help = ''
+
+    def handle(self, *args, **options):
+        all_values = TimeSeriesResultValue.objects.only('value_datetime', 'value_datetime_utc_offset')
+        print('updating %s data values' % all_values.count())
+        for data_value in all_values:
+            data_value.value_datetime = data_value.value_datetime - timedelta(hours=data_value.value_datetime_utc_offset)
+            data_value.save(update_fields=['value_datetime'])
+        print('all values updated')
