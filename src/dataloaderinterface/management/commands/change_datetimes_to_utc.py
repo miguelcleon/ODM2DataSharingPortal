5693b51b24f0703683dddbe10d9d46dc5519aab6
jcaraballo17 <juan.caraballo17@gmail.com>
2018-06-07 17:21:09 -0400

    Updated the data update scripts.

--- src/dataloaderinterface/management/commands/change_datetimes_to_utc.py
+++ src/dataloaderinterface/management/commands/change_datetimes_to_utc.py
@@ -1,4 +1,6 @@
 from datetime import timedelta
+
+import sys
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
@@ -12,6 +14,7 @@ class Command(BaseCommand):
         all_values = TimeSeriesResultValue.objects.only('value_datetime', 'value_datetime_utc_offset')
         print('updating %s data values' % all_values.count())
         for data_value in all_values:
+            sys.stdout.write('.')
             data_value.value_datetime = data_value.value_datetime - timedelta(hours=data_value.value_datetime_utc_offset)
             data_value.save(update_fields=['value_datetime'])
         print('all values updated')
