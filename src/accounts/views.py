428b2871b3e146b9daf100c16d3509e343eee9de
jcaraballo17 <juan.caraballo17@gmail.com>
2018-06-07 12:57:56 -0400

    Fixed user update signal issue where organization name wasn't updated.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import messages
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
@@ -18,6 +18,7 @@ from dataloaderinterface.forms import OrganizationForm
 class UserUpdateView(UpdateView):
     form_class = UserUpdateForm
     template_name = 'registration/account.html'
+    model = get_user_model()
 
     def get_object(self, queryset=None):
         return self.request.user
@@ -45,11 +46,6 @@ class UserUpdateView(UpdateView):
 
         if request.POST.get('disconnect-hydroshare-account'):
             request.user.hydroshare_account.delete()
-            # odm2user = request.user.odm2user
-            # hs_acct_id = odm2user.hydroshare_account.pk
-            # odm2user.hydroshare_account = None
-            # HydroShareAccount.objects.get(pk=hs_acct_id).delete()
-            # odm2user.save()
             form = self.get_form_class()(request.POST, instance=request.user)
 
             context = {
@@ -59,14 +55,10 @@ class UserUpdateView(UpdateView):
             }
             return render(request, self.template_name, context=context)
         else:
-            # user = User.objects.get(pk=request.user.pk)
-            # form = UserUpdateForm(request.POST, instance=user,
-            #                       initial={'organization': user.odm2user.affiliation.organization})
-
             form = self.get_form_class()(request.POST, instance=request.user)
 
             if form.is_valid():
-                form.save()
+                form.instance.save(update_fields=form.changed_data)
                 messages.success(request, 'Your information has been updated successfully.')
                 return HttpResponseRedirect(reverse('user_account'))
             else:
@@ -78,6 +70,7 @@ class UserRegistrationView(CreateView):
     template_name = 'registration/register.html'
     form_class = UserRegistrationForm
     success_url = reverse_lazy('home')
+    model = get_user_model()
 
     def get_context_data(self, **kwargs):
         context = super(UserRegistrationView, self).get_context_data(**kwargs)
